3ca1ed7697c44a855e3a69a356440fabf2835256
mpotisk1 <mpotisk2@gmail.com>
2025-02-09 22:39:54 -0600

    Added a clear button to reset the grid, began creating buttons to change grid size

--- code.js
+++ code.js
@@ -1,18 +1,37 @@
+const container = document.querySelector('.container');
+const box = document.createElement('div');
+const clearButton = document.getElementById('clear');
+
+
+
 function makeGrid(){
-    for (i=0; i < 256; i++){
+    for (i=0; i<256; i++){
     const container = document.querySelector('.container');
     const box = document.createElement('div');
 
     box.classList.add('box');
     box.setAttribute('id', i);
+    box.addEventListener('mouseover', highlight);
+    
     container.appendChild(box);
-  
-
+   
     }
 }
 function highlight(){
-    
+   this.style.cssText = "background-color: black;";
 }
+
+function clear(){
+    const boxes = document.querySelectorAll('.box');
+    for (i=0; i < boxes.length; i++){
+        boxes[i].style.cssText = 'background-color: transparent';
+    }
+    console.log('cleared')
+}
+
+
+clearButton.addEventListener('click', clear);
+
+
 makeGrid();
 
-box.addEventListener('mouseover', highlight());
