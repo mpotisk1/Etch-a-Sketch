8499e76c71864541801093f8d2283d28a2f85e5e
mpotisk1 <mpotisk2@gmail.com>
2025-02-11 16:36:45 -0600

    added a menu to change size of grid

--- code.js
+++ code.js
@@ -1,16 +1,30 @@
 const container = document.querySelector('.container');
 const box = document.createElement('div');
 const clearButton = document.getElementById('clear');
+const sixteen = document.getElementById('sixteen');
+const thirtyTwo = document.getElementById('thirtyTwo');
+const sixtyFour = document.getElementById('sixtyFour');
+const oneHundo = document.getElementById('oneHundo');
+
+const sizeButton = document.getElementById('size');
+sizeButton.addEventListener('click', makeGrid);
+
+
 
 
 
 function makeGrid(){
-    for (i=0; i<256; i++){
     const container = document.querySelector('.container');
+    let gridSize = document.getElementById('size').value;
+    const boxSize = Math.floor(800 / gridSize);
+  
+    container.innerHTML = "";
+    for (i=0; i<gridSize * gridSize; i++){
     const box = document.createElement('div');
-
     box.classList.add('box');
     box.setAttribute('id', i);
+    box.style.width = boxSize + 'px';
+    box.style.height = boxSize + 'px';
     box.addEventListener('mouseover', highlight);
     
     container.appendChild(box);
@@ -18,15 +32,11 @@ function makeGrid(){
     }
 }
 function highlight(){
-   this.style.cssText = "background-color: black;";
+   this.style.backgroundColor = "black";
 }
 
 function clear(){
-    const boxes = document.querySelectorAll('.box');
-    for (i=0; i < boxes.length; i++){
-        boxes[i].style.cssText = 'background-color: transparent';
-    }
-    console.log('cleared')
+    makeGrid();
 }
 
 
